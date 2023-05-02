6cf9fe7d5d71efe2c3b56c9d94d612f2d010ff61
LilNiclas <nicl2995@stud.kea.dk>
2023-05-02 13:13:42 +0200

    Updated SQL script based on ER model

--- src/mysql/init/insert.sql
+++ src/mysql/init/insert.sql
@@ -1,5 +1,17 @@
-USE pmdb;
+USE pct_db;
 
-INSERT INTO project (projectname, duration) VALUES ('WishU', 20);
-INSERT INTO project (projectname, duration) VALUES ('Dolphin', 10);
-INSERT INTO project (projectname, duration) VALUES ('Alpha Solutions', 40);
+INSERT INTO organization (organziation_name, password) VALUES ('Jan´s autofirma', '1234');
+
+INSERT INTO employee (employee_name, email) VALUES ('Jan Mikkelsen', 'janautofirma@live.dk');
+
+INSERT INTO project (project_name, estimated_time, employee_id, organization_id) VALUES ('Project Auto Jan', null, 1, 1);
+
+INSERT INTO task (task_name, start_date, end_date, project_id) VALUES ('Hjulmontering', '2023-05-01', '2023-05-09', 1);
+INSERT INTO task (task_name, start_date, end_date, project_id) VALUES ('Lakering', null, null, 1);
+
+INSERT INTO subtask (subtask_name, start_date, end_date, task_id) VALUES ('Find farvekode', '2023-05-02', '2023-05-03', 2);
+
+INSERT INTO user_task (task_id, employee_id) VALUES (1, 1);
+INSERT INTO user_task (task_id, employee_id) VALUES (2, 1);
+
+INSERT INTO user_subtask (subtask_id, employee_id) VALUES (1, 1);
