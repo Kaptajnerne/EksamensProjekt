5818017935dcb38023906db60788cd40342d45b5
SimonBD1 <simo20k3@stud.kea.dk>
2023-05-04 11:13:03 +0200

    Started on edit project

--- src/main/java/com/example/eksamensprojekt_2sem/Controller/Controller.java
+++ src/main/java/com/example/eksamensprojekt_2sem/Controller/Controller.java
@@ -17,6 +17,15 @@ public class Controller {
     public Controller(RepositoryDB repositoryDB) {
         this.repositoryDB = repositoryDB;
     }
+   /* @GetMapping(path = "")
+    public String startPage (Model model){
+        List<Project> projects = repositoryDB.getProjects();
+        model.addAttribute("projects", projects);
+
+        return "projects";
+    }
+    */
+
 
     //Get projects from org
     //TODO:: Session timer from signin page doesn't translate over to projects. "session.setMaxInactiveInterval(10);"
@@ -25,5 +34,14 @@ public class Controller {
         List<Project> projects = repositoryDB.getProjects(organization_id);
         model.addAttribute("projects", projects);
         return "projects";
+    }
+
+    //edit project
+    @GetMapping(path="/editProject/{project_ID}")
+    public String editProject(@PathVariable("project_ID") int project_ID, Model model) {
+        model.addAttribute("project_id", project_ID);
+        Project project = repositoryDB.getProject(project_ID);
+        model.addAttribute("project", project);
+        return "editProject";
     }
 }
