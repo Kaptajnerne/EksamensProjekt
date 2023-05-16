46f43cb16785d62e14e6c53d6c12adcf9c9df73a
LilNiclas <nicl2995@stud.kea.dk>
2023-05-16 18:40:49 +0200

    get subtask, create subtask, edit subtask with date

--- src/main/java/com/example/eksamensprojekt_2sem/Model/Subtask.java
+++ src/main/java/com/example/eksamensprojekt_2sem/Model/Subtask.java
@@ -1,16 +1,22 @@
 package com.example.eksamensprojekt_2sem.Model;
 
+import java.time.LocalDate;
+
 public class Subtask {
 
     private int subtask_id;
     private String subtask_name;
     private double hours;
+    private LocalDate start_date;
+    private LocalDate end_date;
     private int task_id;
 
-    public Subtask(int subtask_id, String subtask_name, double hours, int task_id) {
+    public Subtask(int subtask_id, String subtask_name, double hours, LocalDate start_date, LocalDate end_date, int task_id) {
         this.subtask_id = subtask_id;
         this.subtask_name = subtask_name;
         this.hours=hours;
+        this.start_date = start_date;
+        this.end_date = end_date;
         this.task_id = task_id;
     }
 
@@ -33,6 +39,30 @@ public class Subtask {
         this.subtask_name = subtask_name;
     }
 
+    public double getHours() {
+        return hours;
+    }
+
+    public void setHours(double hours) {
+        this.hours = hours;
+    }
+
+    public LocalDate getStart_date() {
+        return start_date;
+    }
+
+    public void setStart_date(LocalDate start_date) {
+        this.start_date = start_date;
+    }
+
+    public LocalDate getEnd_date() {
+        return end_date;
+    }
+
+    public void setEnd_date(LocalDate end_date) {
+        this.end_date = end_date;
+    }
+
     public int getTask_id() {
         return task_id;
     }
@@ -41,11 +71,5 @@ public class Subtask {
         this.task_id = task_id;
     }
 
-    public double getHours() {
-        return hours;
-    }
 
-    public void setHours(double hours) {
-        this.hours = hours;
-    }
 }
