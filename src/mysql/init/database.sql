14a04d444fd106c2097f46eab40b1bfb99df0a99
LilNiclas <nicl2995@stud.kea.dk>
2023-06-14 20:44:43 +0200

    Database update. constraints

--- src/mysql/init/database.sql
+++ src/mysql/init/database.sql
@@ -7,39 +7,42 @@ DROP TABLE IF EXISTS project;
 DROP TABLE IF EXISTS user;
 
 CREATE TABLE user
-(user_id int not null auto_increment,
- username varchar(255) not null,
- password varchar(255),
- primary key(user_id));
+(user_id INT NOT NULL auto_increment,
+ username VARCHAR(255) NOT NULL,
+ password VARCHAR(255),
+ PRIMARY KEY(user_id));
 
-CREATE TABLE project
-(project_id int not null auto_increment,
- project_name varchar(255) not null,
- project_description varchar(1000),
- start_date date,
- end_date date,
- user_id int not null,
- primary key(project_id),
- foreign key(user_id) references user (user_id));
+CREATE TABLE project (
+project_id INT NOT NULL AUTO_INCREMENT,
+project_name VARCHAR(255) NOT NULL,
+project_description VARCHAR(1000),
+start_date DATE,
+end_date DATE,
+user_id INT NOT NULL,
+PRIMARY KEY (project_id),
+FOREIGN KEY (user_id) REFERENCES user (user_id)
+);
 
-CREATE TABLE task
-(task_id int not null auto_increment,
- task_name varchar(255),
- hours double,
- start_date date,
- end_date date,
- status int,
- project_id int not null,
- primary key(task_id),
- foreign key(project_id) references project (project_id));
+CREATE TABLE task (
+task_id INT NOT NULL AUTO_INCREMENT,
+task_name VARCHAR(255),
+hours DOUBLE CHECK (hours >= 0 AND hours <= 9999),
+start_date DATE,
+end_date DATE,
+status INT,
+project_id INT NOT NULL,
+PRIMARY KEY (task_id),
+FOREIGN KEY (project_id) REFERENCES project (project_id)
+);
 
-CREATE TABLE subtask
-(subtask_id int not null auto_increment,
- subtask_name varchar(255),
- hours double,
- start_date date,
- end_date date,
- status int,
- task_id int not null,
- primary key(subtask_id),
- foreign key(task_id) references task (task_id));
+CREATE TABLE subtask (
+subtask_id INT NOT NULL AUTO_INCREMENT,
+subtask_name VARCHAR(255),
+hours DOUBLE CHECK (hours >= 0 AND hours <= 9999),
+start_date DATE,
+end_date DATE,
+status INT,
+task_id INT NOT NULL,
+PRIMARY KEY (subtask_id),
+FOREIGN KEY (task_id) REFERENCES task (task_id)
+);
